4ca282a69b9c7b33fe56732f72d775e53fc82093
Jawand Singh <63509680+JawandS@users.noreply.github.com>
2026-01-30 17:05:14 -0500

    Add Git config and build commands to setup script
    Configure Git user details and build Synthea project.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install openjdk-17-jdk -y
+git config user.name "Jawand"
+git config user.email "JawandSingh@gmail.com"
 cd synthea
 ./gradlew build
 ./gradlew build check test
