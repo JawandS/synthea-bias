f698a61a3b47feede99824f223935a8ccc97e0f1
Jawand Singh <63509680+JawandS@users.noreply.github.com>
2026-01-30 17:13:30 -0500

    Modify git config and add clean command
    Updated git config commands to use --global flag and added gradlew clean command.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install openjdk-17-jdk -y
-git config user.name "Jawand"
-git config user.email "JawandSingh@gmail.com"
+git config --global user.name "Jawand"
+git config --global user.email "JawandSingh@gmail.com"
 cd synthea
+./gradlew clean
 ./gradlew build
-./gradlew build check test
